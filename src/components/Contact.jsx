a5a54e0b3038312ddd3d49f09eef4102942f9cda
sonukathat <skkhankathat93@gmail.com>
2025-08-30 07:49:11 +0530

    changes in contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,8 @@ import { LuPhone } from "react-icons/lu";
 function Contact() {
     return (
         <div className="px-8 xl:px-32 bg-[#f9f6ee] py-12">
-            <div className="flex flex-col gap-8 bg-white rounded-4xl px-2 py-8 md:px-8 lg:flex-row lg:justify-around">
-                <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-8 bg-white rounded-4xl px-2 py-8 md:px-8 lg:flex-row lg:justify-between">
+                <div className="flex flex-col gap-6 lg:w-1/2">
                     <div className="flex flex-col justify-center items-center gap-2 bg-green-500 w-52 h-52 p-6 rounded-[50%]">
                         <h2 className="text-5xl font-bold">40%</h2>
                         <p className="font-bold text-lg text-center">BUSSINESS LAUNCH</p>
@@ -21,7 +21,7 @@ function Contact() {
                         <button className="bg-amber-400 px-6 py-3 rounded-4xl">READ MORE</button>
                     </div>
                 </div>
-                <div className="flex flex-col gap-8">
+                <div className="flex flex-col gap-8 lg:w-1/2">
                     <div className="flex flex-col gap-4 md:flex-row">
                         <div className="h-16 w-16 flex justify-center items-center bg-green-400 rounded-[50%]">
                             <MdMailOutline className="" size={44} />
@@ -41,7 +41,7 @@ function Contact() {
                             <p className="font-medium">Come say hello at our office HQ. 121 Rock Sreet, 21 Avenue, New York, NY 92103-9000</p>
                         </div>
                     </div>
-                    <div className="flex flex-col gap-4 md:flex-row md:mt-10">
+                    <div className="flex flex-col gap-4 md:flex-row md:mt-10 lg:mt-0">
                         <div className="h-16 w-16 flex justify-center items-center bg-green-400 rounded-[50%]">
                             <LuPhone className="" size={44} />
                         </div>
