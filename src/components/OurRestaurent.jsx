32b6fe0874b275b5de1fe5d467f47e622349ba6e
sonukathat <skkhankathat93@gmail.com>
2025-10-29 16:47:35 +0530

    update resturant

--- src/components/OurRestaurent.jsx
+++ src/components/OurRestaurent.jsx
@@ -1,12 +1,20 @@
-import restimg from '../assets/pexels-photo-1640773.jpeg'
+import restimg from "../assets/pexels-photo-1640773.jpeg";
 
 function OurRestaurent() {
-    return (
-        <div className='px-8 pt-8 xl:px-32 bg-[#f9f6ee]'>
-            <img className='rounded-4xl min-h-[40rem]' src={restimg} alt="heroimg" />
-            <button className='bg-amber-400 py-4 px-8 text-white font-medium rounded-4xl relative bottom-18 left-2 md:left-7'>READ MORE</button>
-        </div>
-    )
+  return (
+    <div className="px-4 sm:px-8 xl:px-32 pt-8 bg-[#f9f6ee] flex justify-center">
+      <div className="relative w-full max-w-6xl">
+        <img
+          className="w-full rounded-4xl h-[25rem] sm:h-[35rem] lg:h-[40rem] object-cover"
+          src={restimg}
+          alt="restaurant"
+        />
+        <button className="bg-amber-400 py-4 px-8 text-white font-medium rounded-4xl absolute bottom-6 left-4 md:left-8">
+          READ MORE
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default OurRestaurent
+export default OurRestaurent;
