fe26064e11ef981a0841da131f1678c640541583
sonukathat <skkhankathat93@gmail.com>
2025-10-29 16:45:46 +0530

    update hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,20 @@
-import heroimg from '../assets/pexels-ella-olsson-572949-1640777.jpg'
+import heroimg from "../assets/pexels-ella-olsson-572949-1640777.jpg";
 
 function Hero() {
   return (
-    <div className='px-8 pt-8 xl:px-32 bg-[#f9f6ee]'>
-      <img className='rounded-4xl min-h-[40rem]' src={heroimg} alt="heroimg" />
-      <button className='bg-amber-400 py-4 px-8 text-white font-medium rounded-4xl relative bottom-18 left-2 md:left-7'>READ MORE</button>
+    <div className="px-4 sm:px-8 xl:px-32 pt-8 bg-[#f9f6ee] flex justify-center">
+      <div className="relative w-full max-w-6xl">
+        <img
+          className="w-full rounded-4xl h-[25rem] sm:h-[35rem] lg:h-[40rem] object-cover"
+          src={heroimg}
+          alt="heroimg"
+        />
+        <button className="bg-amber-400 py-4 px-8 text-white font-medium rounded-4xl absolute bottom-6 left-4 md:left-8">
+          READ MORE
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
