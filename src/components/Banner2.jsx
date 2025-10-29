05c546a449b5fb1642e324274818104a14a6f398
sonukathat <skkhankathat93@gmail.com>
2025-10-29 16:41:11 +0530

    update btn size

--- src/components/Banner2.jsx
+++ src/components/Banner2.jsx
@@ -4,7 +4,7 @@ function Banner2() {
     return (
         <div className='px-8 pt-8 xl:px-32 bg-[#f9f6ee]'>
             <img className='rounded-4xl min-h-[40rem] lg:mt-6' src={banner2} alt="heroimg" />
-            <button className='bg-amber-400 py-4 px-10 text-white font-medium rounded-4xl relative bottom-20 left-2 md:left-7'>READ MORE</button>
+            <button className='bg-amber-400 py-4 px-8 text-white font-medium rounded-4xl relative bottom-20 left-2 md:left-7'>READ MORE</button>
         </div>
     )
 }
