8ca61767319b9a852e5ce34f9bd582a4ec175bd5
sonukathat <skkhankathat93@gmail.com>
2025-10-29 16:58:01 +0530

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,17 @@
-
 function Footer() {
   return (
     <div className="flex flex-col py-10 bg-[#333333] items-center gap-10">
-      <p className="text-white">Sample text. Click to select the Text Element.</p>
-      <p className="text-sm text-white">This site was created with the <a href="#" className="underline text-white">Nicepage</a></p>
+      <p className="text-white">
+        Sample text. Click to select the Text Element.
+      </p>
+      <p className="text-sm text-white">
+        This site was created with the{" "}
+        <a href="#" className="underline text-white">
+          Nicepage
+        </a>
+      </p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
